5d4c3aef054d6b7f71959a00b4fe197332a97e04
Polina Smirnova <Sp.polinasmirnova@mail.ru>
2025-09-12 17:04:42 +0300

    Update 4. SQL - расчет маркетинговых метрик.sql

--- Проект "Отчет по рекламе Ozon"/4. SQL - расчет маркетинговых метрик.sql
+++ Проект "Отчет по рекламе Ozon"/4. SQL - расчет маркетинговых метрик.sql
@@ -1,20 +1,17 @@
 SELECT 
-    t2.id,
-    t2.campaign_id,
-    t2.title,
-    t2.manager,
     t1.date,
-    t1.views,
-    t1.clicks,
-    t1.spent,
-    t1.basket,
-    t1.orders,
-    t1.orders_money,
-    ROUND((t2.spent / t2.orders_money) * 100, 2) AS ДРР,
-    ROUND(t2.spent / t2.orders, 2) AS CPO,
-    ROUND((t2.spent / t2.views) * 1000, 2) AS CPM,
-    ROUND(t2.spent / t2.clicks, 2) AS CPC,
-    ROUND((t2.orders_money - t2.spent) / t2.spent, 2) AS ROMI
+    SUM(t1.views) AS views,
+    SUM(t1.clicks) AS clicks,
+    SUM(t1.spent_) AS spent,
+    SUM(t1.basket) AS basket,
+    SUM(t1.orders) AS orders,
+    SUM(t1.orders_money_) AS orders_money_,
+    ROUND((SUM(t1.spent_) / SUM(t1.orders_money_)) * 100, 2) AS DRR,
+    ROUND(SUM(t1.spent_) / SUM(t1.orders), 2) AS CPO,
+    ROUND(SUM(t1.spent_) / SUM(t1.views) * 1000, 2) AS CPM,
+    ROUND(SUM(t1.spent_) / SUM(t1.clicks), 2) AS CPC,
+    ROUND((SUM(t1.orders_money_) - SUM(t1.spent_)) / SUM(t1.spent_), 2) AS ROMI
 FROM src.test1_2741000005416 t1
 JOIN src.test2_2741000005417 t2 ON t1.campaign_id = t2.campaign_id
-ORDER BY t1.id ASC;
+GROUP BY t1.date
+ORDER BY t1.date DESC
