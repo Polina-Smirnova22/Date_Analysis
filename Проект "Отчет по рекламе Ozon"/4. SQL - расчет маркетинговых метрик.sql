4f4487253b27331f4c99e7fabb6916f9234e49cf
Polina Smirnova <Sp.polinasmirnova@mail.ru>
2025-09-12 16:53:59 +0300

    Update 4. SQL - расчет маркетинговых метрик.sql

--- Проект "Отчет по рекламе Ozon"/4. SQL - расчет маркетинговых метрик.sql
+++ Проект "Отчет по рекламе Ozon"/4. SQL - расчет маркетинговых метрик.sql
@@ -1,20 +1,20 @@
 SELECT 
-    t1.id,
-    t1.campaign_id,
-    t1.title,
-    t1.manager,
-    t2.date,
-    t2.views,
-    t2.clicks,
-    t2.spent,
-    t2.basket,
-    t2.orders,
-    t2.orders_money,
+    t2.id,
+    t2.campaign_id,
+    t2.title,
+    t2.manager,
+    t1.date,
+    t1.views,
+    t1.clicks,
+    t1.spent,
+    t1.basket,
+    t1.orders,
+    t1.orders_money,
     ROUND((t2.spent / t2.orders_money) * 100, 2) AS ДРР,
     ROUND(t2.spent / t2.orders, 2) AS CPO,
     ROUND((t2.spent / t2.views) * 1000, 2) AS CPM,
     ROUND(t2.spent / t2.clicks, 2) AS CPC,
     ROUND((t2.orders_money - t2.spent) / t2.spent, 2) AS ROMI
-FROM test1 t1
-JOIN test2 t2 ON t1.campaign_id = t2.campaign_id
+FROM src.test1_2741000005416 t1
+JOIN src.test2_2741000005417 t2 ON t1.campaign_id = t2.campaign_id
 ORDER BY t1.id ASC;
